f8cdb8fd84e8fd777846daa4d37030b9329d74cd
Daniel Jung <daniel.j11@gso.schule.koeln>
2022-09-15 12:57:51 +0200

    Projektdateien hinzufügen.

---
+++ 2DatentypenKonvertierung/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Aufgabe 1*/
+
+Console.WriteLine($"size of short: {short.MaxValue}");
+Console.WriteLine($"size of short: {short.MinValue}");
+/*
+ * Bit 16
+ * size of short: 32767
+ * size of short: -32767
+ */
+Console.WriteLine($"size of int: {int.MaxValue}");
+Console.WriteLine($"size of int: {int.MinValue}");
+/*
+ * Bit 32
+ * size of int: 2147483647
+ * size of int: -2147483647
+ */
+Console.WriteLine($"size of long: {long.MaxValue}");
+Console.WriteLine($"size of long: {long.MinValue}");
+/*
+ * Bit 64
+ * size of long: 9223372036854775808
+ * size of long: -9223372036854775808
+ */
+Console.WriteLine($"size of float: {float.MaxValue}");
+Console.WriteLine($"size of float: {float.MinValue}");
+/*
+ * Byte 4
+ * size of float: 3,4028235E+38
+ * size of float: -3,4028235E+38
+ */
+Console.WriteLine($"size of double: {double.MaxValue}");
+Console.WriteLine($"size of double: {double.MinValue}");
+/*
+ * Byte 8
+ * size of double: 1,7976931348623157E+308
+ * size of double: -1,7976931348623157E+308
+ */
+Console.WriteLine($"size of decimal: {decimal.MaxValue}");
+Console.WriteLine($"size of decimal: {decimal.MinValue}");
+/*
+ * Bit 10
+ * size of decimal: 79228162514264337593543950335
+ * size of decimal: -79228162514264337593543950335
+ */
+
+/*Aufgabe 2*/
+
+string Vorname = "Daniel";
+short Alter = 17;
+bool Frage = true;
+
+/*Aufgabe 3*/
+
+int DollarZahl = 36;
+char Dollar = DollarZahl;
